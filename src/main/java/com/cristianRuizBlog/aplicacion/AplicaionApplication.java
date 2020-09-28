3211fce5712852b0967b2b6cfc0b74cadde3bfc8
Sergio Sacristan <>
2020-09-28 20:45:06 +0200

    heroku config

--- src/main/java/com/cristianRuizBlog/aplicacion/AplicaionApplication.java
+++ src/main/java/com/cristianRuizBlog/aplicacion/AplicaionApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.cristianRuizBlog.aplicacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AplicaionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AplicaionApplication.class, args);
 	}
 
 }
